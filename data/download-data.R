b258eddfc48653f1aa8ead5e9fba2d92637ca24f
LVesters <l.vesters@tilburguniversity.edu>
2025-09-10 14:54:28 +0200

    Editing download-data.R and .gitignore

--- data/download-data.R
+++ data/download-data.R
@@ -22,3 +22,16 @@ title_basics <- readr::read_tsv("data/raw_data/title.basics.tsv") %>%
   filter(titleType == "movie") %>%
   select(tconst, runtimeMinutes, genres)
 
+# Selecting movies
+title_ratings_movies <- title_ratings %>%
+  semi_join(title_basics, by = "tconst")
+title_crew_movies <- title_crew %>% 
+  semi_join(title_basics, by = "tconst")
+
+# Merge into one dataframe
+imdb_movies <- title_basics %>%
+  left_join(title_ratings_movies, by = "tconst") %>%
+  left_join(title_crew_movies, by = "tconst")
+
+# Save as csv 
+readr::write_csv(imdb_movies, "data/imdb_movies_dataset.csv")
