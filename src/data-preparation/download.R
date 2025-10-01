2e4fcf6845e6413f0e0921b4e860100bbcd09774
Seppe Bogers <seppebogers@ww020358.uvt.nl>
2025-10-01 13:58:24 +0200

    I improved the download.R file

--- src/data-preparation/download.R
+++ src/data-preparation/download.R
@@ -1,27 +1,25 @@
-#required packages
-load_pkg <- function(pkg){
-  if(!require(pkg, character.only = TRUE)){
-    install.packages(pkg, dependencies = TRUE)
-    library(pkg, character.only = TRUE)
-  }
-}
+# SETUP:
+# Installing and loading required packages
+install.packages(c("readr", "tidyverse"))
+
 packages <- c("readr", "tidyverse")
-lapply(packages, load_pkg)
+lapply(packages, library, character.only = TRUE)
 
-#set directory
-data_dir <- "../../data/raw_data"
-
-# Creating folder
+# Creating folder raw data
 if(!dir.exists("data/raw_data")){
   dir.create("data/raw_data", recursive = TRUE)
 }
 
+# Set working directory
+setwd("../../data/raw_data")
+
+# INPUT:
 # URLs for the IMDb datasets
 urls <- list(
-  ratings = "https://datasets.imdbws.com/title.ratings.tsv.gz",
-  crew = "https://datasets.imdbws.com/title.crew.tsv.gz",
-  name_basics = "https://datasets.imdbws.com/name.basics.tsv.gz",
-  title_basics = "https://datasets.imdbws.com/title.basics.tsv.gz"
+  title.ratings = "https://datasets.imdbws.com/title.ratings.tsv.gz",
+  title.crew = "https://datasets.imdbws.com/title.crew.tsv.gz",
+  name.basics = "https://datasets.imdbws.com/name.basics.tsv.gz",
+  title.basics = "https://datasets.imdbws.com/title.basics.tsv.gz"
 )
 
 for(name in names(urls)){
@@ -31,10 +29,23 @@ for(name in names(urls)){
   }
 }
 
-# Naming the files
-title_ratings <- readr::read_tsv("data/raw_data/ratings.tsv.gz")
-title_crew <- readr::read_tsv("data/raw_data/crew.tsv.gz")
-name_basics <- readr::read_tsv("data/raw_data/name_basics.tsv.gz")
-title_basics <- readr::read_tsv("data/raw_data/title_basics.tsv.gz") %>% 
+# TRANSFORMATION:
+# Naming the files in environment
+title_ratings <- readr::read_tsv("data/raw_data/title.ratings.tsv.gz")
+title_crew <- readr::read_tsv("data/raw_data/title.crew.tsv.gz")
+name_basics <- readr::read_tsv("data/raw_data/name.basics.tsv.gz")
+title_basics <- readr::read_tsv("data/raw_data/title.basics.tsv.gz") %>% 
   filter(titleType == "movie") %>% 
   select(tconst, runtimeMinutes, genres, startYear)
+
+# OUTPUT
+# Creating folder processed data
+if(!dir.exists("data/processed")){
+  dir.create("data/processed", recursive = TRUE)
+}
+
+# Save cleaned datasets as .csv
+readr::write_csv(title_ratings, "data/processed/title_ratings_clean.csv")
+readr::write_csv(title_crew, "data/processed/title_crew_clean.csv")
+readr::write_csv(name_basics, "data/processed/name_basics_clean.csv")
+readr::write_csv(title_basics, "data/processed/title_basics_clean.csv")
