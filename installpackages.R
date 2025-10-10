5c2b3eea9bb9d4be1aa2f36469a448621348ba4d
Seppe Bogers <seppebogers@macbookair-1.home>
2025-10-10 12:24:28 +0200

    made final adjustments to installpackages.R

--- installpackages.R
+++ installpackages.R
@@ -1,3 +1,3 @@
 # Install.packages script for all necessary packages
-install.packages(c("readr", "tidyverse", "dplyr", "ggplot2", "kableExtra"))
-
+install.packages(c("readr", "tidyverse", "dplyr", "ggplot2", "kableExtra", "tinytex"))
+tinytex::install_tinytex()
